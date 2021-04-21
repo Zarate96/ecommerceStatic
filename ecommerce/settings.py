88c4adfa8c44e25f94c15abce441cb96c43bb79c
Your Name <you@example.com>
2021-04-20 23:44:33 -0500

    Templates

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -136,6 +136,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 #whitenoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
