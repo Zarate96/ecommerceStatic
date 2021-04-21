44a8d155a3a68708f34f2c243712656e3049d0a8
Your Name <you@example.com>
2021-04-21 13:38:08 -0500

    Templates

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'website/home.html', {})
+    return render(request, 'home.html', {})
 
 def blog(request):
-    return render(request, 'website/blog.html', {})
+    return render(request, 'blog.html', {})
 
 def blogDetails(request):
-    return render(request, 'website/blog-details.html', {})
+    return render(request, 'blog-details.html', {})
