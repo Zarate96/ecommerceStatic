3ca0e4183d5fa726189f6f6da60104fa3dc689fe
Your Name <you@example.com>
2021-04-21 13:59:45 -0500

    Templates

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -2,6 +2,8 @@ import os
 import django_heroku
 import dj_database_url
 from decouple import config
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
