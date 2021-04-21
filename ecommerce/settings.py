fd2029499da20be421f4edb8f5046555023d066e
Your Name <you@example.com>
2021-04-21 00:21:03 -0500

    Templates

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,12 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #whitenoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
